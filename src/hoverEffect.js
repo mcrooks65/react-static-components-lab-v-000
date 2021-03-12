ec9630d5727d5df3e85a8e48918fedfc2dfc7f5b
Matthew Crooks <mfcrooks@gmail.com>
2021-03-12 00:10:17 +0000

    Automatically backed up by Learn

--- src/hoverEffect.js
+++ src/hoverEffect.js
@@ -1,5 +1,5 @@
 // This script is an altered version of the CodePen made by 'Captain Anonymous': https://codepen.io/anon/pen/jKQoaj
-
+/* eslint-disable */
 const rippleSettings = {
   maxSize: 100,
   animationSpeed: 2,
