2d335f312c649089be63a274e74294641f07e6eb
Matthew Crooks <mfcrooks@gmail.com>
2021-03-12 00:06:25 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import CatComponent from './CatComponent'
-// import EinsteinQuoteComponent from './EinsteinQuoteComponent'
-// import MouseComponent from './MouseComponent'
+import CatComponent from './CatComponent'
+import EinsteinQuoteComponent from './EinsteinQuoteComponent'
+import MouseComponent from './MouseComponent'
 
 class App extends Component {
 	render() {
